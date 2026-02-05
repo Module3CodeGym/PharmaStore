8c1bc2dbc1105235a19c72340da133f881b17187
KhanhAn-Student <anngockhanh0145@gmail.com>
2026-02-05 12:47:38 +0700

    Delete src directory

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AppRouter from './routes/AppRouter';
-import './index.css';
-
-function App() {
-  return <AppRouter />;
-}
-
-export default App;
--- src/components/common/index.js
+++
@@ -1,5 +0,0 @@
-// Common Components Exports
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Modal } from './Modal';
-export { default as Table } from './Table';
